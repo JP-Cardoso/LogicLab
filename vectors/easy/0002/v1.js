a2f29bc0639f488eb05a67e7bf31d62828341403
jp-cardoso <jhonpoal@hotmail.com>
2025-02-10 21:53:42 -0300

    feat: implementing second version

--- vectors/easy/0002/v1.js
+++ vectors/easy/0002/v1.js
@@ -2,13 +2,25 @@
  Declare um vetor de 10 posições e o preencha com os 10 primeiros números impares e o escreva.
  */
 
-const vector = Array.from({length: 100}, (_, i) => i);
+import { Utils } from "../../../utils/index.js";
+
+const vector = Array.from({ length: 100 }, (_, i) => i);
 
 const newArr = []
 for (let index = 0; newArr.length < 10; index++) {
   if (vector[index] % 2 === 1) {
     newArr.push(vector[index]);
-  }  
+  }
 }
 
-console.log(newArr);
+Utils.log({ msg: "First version" });
+Utils.log({ info: newArr })
+
+Utils.separator();
+
+Utils.log({ msg: "Second version" });
+const vectorFilter = Array.from({ length: 100 }, (_, i) => i)
+  .filter(n => n % 2 === 1)
+  .slice(0, 10);
+Utils.log({ info: vectorFilter })
+Utils.separator();
